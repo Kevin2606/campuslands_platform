9acbe92e8ef857a934eae93deb202c399ec36eb3
Kevin2606 <andreskevin2606@gmail.com>
2023-10-04 10:19:22 -0500

    feat: :lipstick: responsive design added to home

--- src/components/home/homePage/CardCursos.jsx
+++ src/components/home/homePage/CardCursos.jsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom";
+import { Button } from "@nextui-org/react";
+
 
 export const CardCursos = ({nameCourse, description, patch, srcImg}) => {
+    /* flex flex-col items-center border p-10 w-1/2 sm:w-1/4 md:w-1/4 lg:w-1/4 xl:w-2/5 */
     return (
-        <div className="flex flex-col items-center border p-10 w-1/2 sm:w-1/4 md:w-1/4 lg:w-1/4 xl:w-2/5">
-            <h1 className="w-full text-center">{nameCourse}</h1>
+        <div className="flex flex-col items-center border p-10 gap-5 lg:w-1/3 ">
+            <h1 className="w-full text-center text-2xl font-medium">{nameCourse}</h1>
             <div>
-                <p>{description}</p>
+                <p className="text-lg">{description}</p>
             </div>
             <div>
                 <Link to={patch}>
-                    <button>Ver</button>
+                    <Button color="warning">Ver</Button>
                 </Link>
             </div>
         </div>
--- src/components/home/homePage/HomePage.jsx
+++ src/components/home/homePage/HomePage.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { CardCursos } from "./CardCursos";
 import { Navigator } from "../../navigator/Navigator";
 import { Link } from "react-router-dom";
-
+import { Button } from "@nextui-org/react";
 
 export const HomePage = () => {
     const dataHome = {
-        nameLastCourse : "Nombre del curso",
-        imgLastCourse : "https://placehold.co/400",
-        pathLastCourse : "/videos/pruebita",
-        courses : [
+        nameLastCourse: "Nombre del curso",
+        imgLastCourse: "https://placehold.co/400",
+        pathLastCourse: "/videos/pruebita",
+        courses: [
             {
                 nameCourse: "Nombre del curso1",
                 description: "Breve descripcion del curso",
@@ -57,35 +57,35 @@ export const HomePage = () => {
                 description: "Breve descripcion del curso",
                 patch: "/videos/pruebita",
                 srcImg: "https://placehold.co/400",
-            }
-        ]
-    }
+            },
+        ],
+    };
     return (
         <>
             <div>
                 <Navigator />
-                <main className="border m-10 mt-0 mb-0 pb-10">
-                    <div className="flex flex-row justify-around border-b-1">
-                        <div className="flex flex-col justify-center gap-y-20">
-                            <h1 className="text-5xl">{dataHome.nameLastCourse}</h1>
-                            <Link to={dataHome.pathLastCourse} >
-                            <button className="border">Continuar/Ver</button>
-                            </Link>
+                {/* border m-10 mt-0 mb-0 pb-10 */}
+                <main className="pb-10 2xl:border 2xl:m-10 2xl:mt-0 2xl:mb-0">
+                    {/* flex flex-row justify-around border-b-1*/}
+                    <div className="flex flex-col justify-center items-center border-b-1 lg:flex-row lg:gap-x-10 2xl:justify-around">
+                        <div className="w-fit">
+                            <img width={"100%"} height={"100%"} src={dataHome.imgLastCourse} alt="" />
                         </div>
-                        <div>
-                            <img src={dataHome.imgLastCourse} alt="" />
+                        <div className="flex flex-col justify-center items-center gap-y-10 p-5">
+                            <h1 className="text-5xl text-center">
+                                {dataHome.nameLastCourse}
+                            </h1>
+                            <Link to={dataHome.pathLastCourse}>
+                                <Button color="warning">Ver</Button>
+                            </Link>
                         </div>
                     </div>
                     <div className="flex flex-col items-center gap-5">
                         <h1 className="p-5 text-4xl">Cursos</h1>
                         <div className="flex flex-row flex-wrap justify-center flex-shrink gap-10">
-                            {
-                                dataHome.courses.map((course, index) => {
-                                    return (
-                                        <CardCursos key={index} {...course} />
-                                    )
-                                })
-                            }
+                            {dataHome.courses.map((course, index) => {
+                                return <CardCursos key={index} {...course} />;
+                            })}
                         </div>
                     </div>
                 </main>
