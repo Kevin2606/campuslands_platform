d3549696495c75e686a4292dee04e75cab14db0e
Kevin2606 <andreskevin2606@gmail.com>
2023-10-04 09:25:17 -0500

    feat: :sparkles: props are added to the main view of the project

--- src/components/home/homePage/CardCursos.jsx
+++ src/components/home/homePage/CardCursos.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 
-export const CardCursos = () => {
+export const CardCursos = ({nameCourse, description, patch, srcImg}) => {
     return (
         <div className="flex flex-col items-center border p-10 w-1/2 sm:w-1/4 md:w-1/4 lg:w-1/4 xl:w-2/5">
-            <h1 className="w-full text-center">Javascript Experto</h1>
+            <h1 className="w-full text-center">{nameCourse}</h1>
             <div>
-                <p>Breve descripcion del curso</p>
+                <p>{description}</p>
             </div>
             <div>
-                <Link to="/videos/pruebita">
+                <Link to={patch}>
                     <button>Ver</button>
                 </Link>
             </div>
--- src/components/home/homePage/HomePage.jsx
+++ src/components/home/homePage/HomePage.jsx
@@ -1,8 +1,65 @@
 import React from "react";
 import { CardCursos } from "./CardCursos";
 import { Navigator } from "../../navigator/Navigator";
+import { Link } from "react-router-dom";
+
 
 export const HomePage = () => {
+    const dataHome = {
+        nameLastCourse : "Nombre del curso",
+        imgLastCourse : "https://placehold.co/400",
+        pathLastCourse : "/videos/pruebita",
+        courses : [
+            {
+                nameCourse: "Nombre del curso1",
+                description: "Breve descripcion del curso",
+                patch: "/videos/pruebita",
+                srcImg: "https://placehold.co/400",
+            },
+            {
+                nameCourse: "Nombre del curso",
+                description: "Breve descripcion del curso",
+                patch: "/videos/pruebita",
+                srcImg: "https://placehold.co/400",
+            },
+            {
+                nameCourse: "Nombre del curso",
+                description: "Breve descripcion del curso",
+                patch: "/videos/pruebita",
+                srcImg: "https://placehold.co/400",
+            },
+            {
+                nameCourse: "Nombre del curso",
+                description: "Breve descripcion del curso",
+                patch: "/videos/pruebita",
+                srcImg: "https://placehold.co/400",
+            },
+            {
+                nameCourse: "Nombre del curso",
+                description: "Breve descripcion del curso",
+                patch: "/videos/pruebita",
+                srcImg: "https://placehold.co/400",
+            },
+            {
+                nameCourse: "Nombre del curso",
+                description: "Breve descripcion del curso",
+                patch: "/videos/pruebita",
+                srcImg: "https://placehold.co/400",
+            },
+            {
+                nameCourse: "Nombre del curso",
+                description: "Breve descripcion del curso",
+                patch: "/videos/pruebita",
+                srcImg: "https://placehold.co/400",
+            },
+            {
+                nameCourse: "Nombre del curso",
+                description: "Breve descripcion del curso",
+                patch: "/videos/pruebita",
+                srcImg: "https://placehold.co/400",
+            }
+        ]
+    }
     return (
         <>
             <div>
@@ -10,26 +67,26 @@ export const HomePage = () => {
                 <main className="border m-10 mt-0 mb-0 pb-10">
                     <div className="flex flex-row justify-around border-b-1">
                         <div className="flex flex-col justify-center gap-y-20">
-                            <h1 className="text-5xl">Nombre del curso</h1>
+                            <h1 className="text-5xl">{dataHome.nameLastCourse}</h1>
+                            <Link to={dataHome.pathLastCourse} >
                             <button className="border">Continuar/Ver</button>
+                            </Link>
                         </div>
                         <div>
-                            <img src="https://placehold.co/400" alt="" />
+                            <img src={dataHome.imgLastCourse} alt="" />
                         </div>
                     </div>
                     <div className="flex flex-col items-center gap-5">
                         <h1 className="p-5 text-4xl">Cursos</h1>
                         <div className="flex flex-row flex-wrap justify-center flex-shrink gap-10">
-                            <CardCursos />
-                            <CardCursos />
-                            <CardCursos />
-                            <CardCursos />
-                            <CardCursos />
-                            <CardCursos />
-                            <CardCursos />
-                            <CardCursos />
+                            {
+                                dataHome.courses.map((course, index) => {
+                                    return (
+                                        <CardCursos key={index} {...course} />
+                                    )
+                                })
+                            }
                         </div>
-
                     </div>
                 </main>
             </div>
