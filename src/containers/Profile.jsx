6815a7a14103788e6616e40670def132e96be4a6
Kevin2606 <andreskevin2606@gmail.com>
2023-10-05 12:02:16 -0500

    feat: :sparkles: add new feature in profile

--- src/containers/Profile.jsx
+++ src/containers/Profile.jsx
@@ -21,13 +21,14 @@ export const Profile = () => {
                 <Navigator />
                 <div className="bg-[#222831]">
                     <section className="flex justify-center">
-                        <Atropos shadow={false} shadowOffset={0}>
-                            <div className="flex flex-col items-center justify-center">
+                        <div className="flex flex-col items-center justify-center">
+                            <Atropos shadow={false} shadowOffset={0} className="w-fit relative top-20 rounded-full">
                                 <div
                                     style={{
                                         border: `8px solid ${user.banner_color}`,
                                     }}
-                                    className="w-fit relative top-20 rounded-full"
+                                    className="rounded-full"
+                                    
                                 >
                                     <img
                                         className="rounded-full"
@@ -35,47 +36,45 @@ export const Profile = () => {
                                         alt="Imagen de perfil"
                                     />
                                 </div>
+                            </Atropos>
+                            <div
+                                style={{
+                                    border: `8px solid ${user.banner_color}`,
+                                    backgroundColor: `${user.banner_color}33`,
+                                }}
+                                className={`flex flex-col rounded-2xl items-center gap-7 p-10 pt-28`}
+                            >
+                                <h2 className="text-xl">@{user.username}</h2>
+                                <h1 className="text-2xl">{nick}</h1>
                                 <div
                                     style={{
-                                        border: `8px solid ${user.banner_color}`,
-                                        backgroundColor: `${user.banner_color}33`,
+                                        borderColor: `${user.banner_color}`,
                                     }}
-                                    className={`flex flex-col rounded-2xl items-center gap-7 p-10 pt-28`}
+                                    className="w-56 lg:w-96 flex flex-row flex-wrap gap-5 border-y-5 p-5 items-center justify-center"
                                 >
-                                    <h2 className="text-xl">
-                                        @{user.username}
-                                    </h2>
-                                    <h1 className="text-2xl">{nick}</h1>
-                                    <div
-                                        style={{
-                                            borderColor: `${user.banner_color}`,
-                                        }}
-                                        className="w-56 lg:w-96 flex flex-row flex-wrap gap-5 border-y-5 p-5 items-center justify-center"
-                                    >
-                                        {roles.map((role, index) => {
-                                            return (
-                                                <p
-                                                    key={index}
-                                                    className="text-lg italic"
-                                                >
-                                                    {rolDC[role]}
-                                                </p>
-                                            );
-                                        })}
-                                    </div>
-                                    <Button
-                                        style={{
-                                            border: `3px solid ${user.banner_color}`,
-                                        }}
-                                        variant="bordered"
-                                        className="text-lg"
-                                        onPress={handleLogout}
-                                    >
-                                        Cerrar sesion
-                                    </Button>
+                                    {roles.map((role, index) => {
+                                        return (
+                                            <p
+                                                key={index}
+                                                className="text-lg italic"
+                                            >
+                                                {rolDC[role]}
+                                            </p>
+                                        );
+                                    })}
                                 </div>
+                                <Button
+                                    style={{
+                                        border: `3px solid ${user.banner_color}`,
+                                    }}
+                                    variant="bordered"
+                                    className="text-lg"
+                                    onPress={handleLogout}
+                                >
+                                    Cerrar sesion
+                                </Button>
                             </div>
-                        </Atropos>
+                        </div>
                     </section>
                 </div>
             </div>
